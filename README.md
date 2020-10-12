1150016c757d8d2cb77b7952234da3db556e0aba
Juan Manuel Serruya <shuank@gmail.com>
2020-10-12 14:41:25 +0200

    first version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## A clean, beautiful and responsive portfolio template for Developers
 
 <h2 align="center">
-  <img src="https://github.com/cobidev/gatsby-simplefolio/blob/master/examples/example.gif" alt="Gatsby Simplefolio" width="600px" />
+  <img src="https://github.com/cobidev/gatsby-simplefolio/blob/master/examples/example.gif" alt="Juan Manuel Serruya" width="600px" />
   <br>
 </h2>
 
@@ -95,7 +95,7 @@ $ sudo npm install --unsafe-perm=true --allow-root
 Once your server has started, go to this url `http://localhost:8000/` and you will see the website running on a Development Server:
 
 <h2 align="center">
-  <img src="https://github.com/cobidev/gatsby-simplefolio/blob/master/examples/example.png" alt="Gatsby Simplefolio" width="100%">
+  <img src="https://github.com/cobidev/gatsby-simplefolio/blob/master/examples/example.png" alt="Juan Manuel Serruya" width="100%">
 </h2>
 
 ---
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,46 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Juan Manuel Serruya | Angel Investor', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to the homepage of Juan Manuel Serruya, Angel Investor based in Europe.', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Juan Manuel Serruya',
+  subtitle: 'I\'m an Angel Investor backing early stage technology companies',
+  cta: 'Learn More',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Hi! I am a technologist that spent the last two decades building software as an engineer and engineering leader. I recently left my job as at Spotify after 6 years, there, I lead product organisation of 60 folks and experienced what it\'s like to grow at tremendous speed and eventually execute a multi billion dollar IPO',
+  paragraphTwo: 'Currently I\'m the Founder and CEO of Datia, a technology company that has the vision to work to accelerate the world\'s transition towards sustainable finance.',
+  paragraphThree: 'I am investing in early stage tech companies at the pre seed and seed stage, my check size is 25-50K EUR and my main contribution to you besides money is to help you build a world class and product organisation.',
+  resume: 'https://www.linkedin.com/in/jmserruya/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    img: 'azelio.jpg',
+    title: 'Azelio',
+    info: 'CLEAN POWER WHEN & WHERE YOU NEED IT',
+    info2: 'Combining an innovative storage design with well-established power production technology, Azelio has developed a renewable energy storage solution providing around-the-clock clean power.',
+    url: 'https://www.azelio.com/',
+    repo: '', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'hello@juan.vc',
 };
 
 // FOOTER DATA
@@ -68,27 +50,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/JMSerruya',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'github',
-      url: '',
+      url: 'https://www.linkedin.com/in/jmserruya/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
