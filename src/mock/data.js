a8f0cc857257285fbfdc21761a8f4206d351e271
Juan Manuel Serruya <shuank@gmail.com>
2020-10-12 14:56:36 +0200

    fix typo

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,19 @@ export const headData = {
 export const heroData = {
   title: 'Hi, my name is',
   name: 'Juan Manuel Serruya',
-  subtitle: 'I\'m an Angel Investor backing early stage technology companies',
+  subtitle: "I'm an Angel Investor backing early stage technology companies",
   cta: 'Learn More',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'Hi! I am a technologist that spent the last two decades building software as an engineer and engineering leader. I recently left my job as at Spotify after 6 years, there, I lead product organisation of 60 folks and experienced what it\'s like to grow at tremendous speed and eventually execute a multi billion dollar IPO',
-  paragraphTwo: 'Currently I\'m the Founder and CEO of Datia, a technology company that has the vision to work to accelerate the world\'s transition towards sustainable finance.',
-  paragraphThree: 'I am investing in early stage tech companies at the pre seed and seed stage, my check size is 25-50K EUR and my main contribution to you besides money is to help you build a world class and product organisation.',
+  paragraphOne:
+    "Hi! I am a technologist that spent the last two decades building software as an engineer and engineering leader. I recently left my job at Spotify after 6 years, there, I lead product organisation of 60 folks and experienced what it's like to grow at tremendous speed and eventually execute a multi billion dollar IPO",
+  paragraphTwo:
+    "Currently I'm the Founder and CEO of Datia, a technology company that has the vision to work to accelerate the world's transition towards sustainable finance.",
+  paragraphThree:
+    'I am investing in early stage tech companies at the pre seed and seed stage, my check size is 25-50K EUR and my main contribution to you besides money is to help you build a world class and product organisation.',
   resume: 'https://www.linkedin.com/in/jmserruya/', // if no resume, the button will not show up
 };
 
@@ -31,10 +34,11 @@ export const projectsData = [
     img: 'azelio.jpg',
     title: 'Azelio',
     info: 'CLEAN POWER WHEN & WHERE YOU NEED IT',
-    info2: 'Combining an innovative storage design with well-established power production technology, Azelio has developed a renewable energy storage solution providing around-the-clock clean power.',
+    info2:
+      'Combining an innovative storage design with well-established power production technology, Azelio has developed a renewable energy storage solution providing around-the-clock clean power.',
     url: 'https://www.azelio.com/',
     repo: '', // if no repo, the button will not show up
-  }
+  },
 ];
 
 // CONTACT DATA
